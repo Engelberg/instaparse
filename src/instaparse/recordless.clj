60e5e179ebd9cedf79911777d3340e0509918d69
Mark Engelberg <mark.engelberg@gmail.com>
2013-02-12 00:15:50 -0800

    New technique working.

--- src/instaparse/recordless.clj
+++ src/instaparse/recordless.clj
@@ -1,9 +1,18 @@
-(ns instaparse.recordless)
-; Don't push onto stack things parsing tasks that have been previously pushed
+(ns instaparse.recordless
+  (:use clojure.pprint))
 
-(defprotocol Parser
-   ;; parse a text starting at index using a given trampoline structure
-  (-parse [this index tramp]))
+(defn get-parser [grammar p]
+  (get grammar p p))
+
+(declare alt-parse cat-parse string-parse epsilon-parse end-parse non-terminal-parse)
+(defn uparse [grammar parser index tramp]
+  (if (keyword? parser) (non-terminal-parse grammar parser index tramp)
+    (case (:tag parser)
+      :alt (alt-parse grammar parser index tramp)
+      :cat (cat-parse grammar parser index tramp)
+      :string (string-parse parser index tramp)
+      :epsilon (epsilon-parse index tramp)
+      :end (end-parse index tramp))))
 
 ; The trampoline structure contains the text to parse, a stack and a nodes
 ; text is a string
@@ -20,45 +29,31 @@
 (defn total-success? [tramp s]
   (= (count (:text tramp)) (:index s)))
 
-; Task is the protocol used by items on the stack
-
-(defprotocol Task (execute [_ tramp]))
-
-; NotificationTask is one kind of thing that can live on the stack
-; A NotificationTask is comprised of a listener and a result to send to the listener. 
-
-(defrecord Notification [listener result]
-  Task
-  (execute [_ tramp] (listener result tramp)))
-
-; ParseTask is another thing that can live on the stack
-
-(defrecord ParseTask [parser index]
-  Task
-  (execute [_ tramp] (-parse parser index tramp)))
-
 ; The trampoline's nodes field is map from [index parser] pairs to Nodes
 ; Nodes track the results of a given parser at a given index, and the listeners
 ; who care about the result.
 ; Both results and listeners are expected to be refs of sets.
 
-(defrecord Node [listeners results])
-(defn make-node [] (Node. (ref #{}) (ref #{})))
+(defn make-node [] {:listeners (atom #{}) :results (atom #{})})
 
 ;; Trampoline helper functions
+
+;(defn push-stack-cached
+;  "Pushes an item onto the trampoline's stack"
+;  [tramp item]
+;  (when (not (@(:prev-pushed tramp) item))
+;    (swap! (:stack tramp) conj item)
+;    (swap! (:prev-pushed tramp) conj item)))
 
 (defn push-stack
   "Pushes an item onto the trampoline's stack"
   [tramp item]
-  (swap! (:stack tramp) conj item)) 
+  (swap! (:stack tramp) conj item))
 
 (defn node-get
   "Gets node if already exists, otherwise creates one"
   [tramp node-key]
-  (let [nodes (:nodes tramp)
-        node-key (if (instance? clojure.lang.Var (node-key 1))
-                   [(node-key 0) @(node-key 1)]
-                   node-key)]
+  (let [nodes (:nodes tramp)]
     (if-let [node (@nodes node-key)]
       node 
       (let [node (make-node)]
@@ -69,25 +64,24 @@
   "Pushes a result into the trampoline's node.
    Schedules notification to all existing listeners of result."
   [tramp node-key result]
-  (dosync
-    (let [node (node-get tramp node-key)
-          results (:results node)]
-      (when (not (@results result))  ; when result is not already in @results
-        (alter results conj result)
-        (doseq [listener @(:listeners node)]
-          (push-stack tramp (Notification. listener result))))))) 
+  (let [node (node-get tramp node-key)
+        results (:results node)]
+    (when (not (@results result))  ; when result is not already in @results
+      (swap! results conj result)
+      (doseq [listener @(:listeners node)]
+        (push-stack tramp #(listener result)))))) 
 
 (defn push-listener
   "Pushes a listener into the trampoline's node.
    Schedules notification to listener of all existing results."
   [tramp node-key listener]
-  (dosync
-    (let [node (node-get tramp node-key)
-          listeners (:listeners node)]
-      (when (not (@listeners listener))  ; when listener is not already in listeners
-        (alter listeners conj listener)
-        (doseq [result @(:results node)]
-          (push-stack tramp (Notification. listener result))))))) 
+  (let [node (node-get tramp node-key)
+        listeners (:listeners node)]
+    (when (not (@listeners listener))  ; when listener is not already in listeners
+      (swap! listeners conj listener)
+      (doseq [result @(:results node)]
+        (push-stack tramp #(listener result)))
+      true))) 
 
 (defn success [tramp node-key result end]
   (push-result tramp node-key (make-success result end)))
@@ -112,17 +106,19 @@
 
 (defn step
   "Executes one thing on the stack (not threadsafe)"
-  [tramp]
-  (let [stack (:stack tramp), top (peek @stack)]
+  [stack]
+  (let [top (peek @stack)]
     (swap! stack pop)
-    (execute top tramp)))
+    (top)))
 
 (defn run
   "Executes the stack until exhausted"
   [tramp]
   (let [stack (:stack tramp)]
     (while (pos? (count @stack))
-      (step tramp))))
+      (step stack)
+      (pprint stack)
+      )))
 
 ;; Listeners
 
@@ -130,8 +126,8 @@
 ; The first kind is a NodeListener which simply listens for a completed parse result
 ; Takes the node-key of the parser which is awaiting this result.
 
-(defn NodeListener [node-key]
-  (fn [result tramp] (push-result tramp node-key result)))
+(defn NodeListener [node-key tramp]
+  (fn [result] (push-result tramp node-key result)))
 
 ; The second kind of listener is a CatListener which listens at each stage of the
 ; concatenation parser to carry on the next step.  Think of it as a parse continuation.
@@ -139,78 +135,89 @@
 ; before, and a list of parsers that remain.  Also, the node-key of the final node
 ; that needs to know the overall result of the cat parser.
 
-(defn CatListener [results-so-far parser-sequence node-key]
-  (fn [result tramp] 
+(defn CatListener [results-so-far parser-sequence node-key tramp grammar]
+  (fn [result] 
     (let [{parsed-result :result continue-index :index} result
           new-results-so-far (conj results-so-far parsed-result)]
       (if (seq parser-sequence)
-        (do (push-listener tramp [continue-index (first parser-sequence)]
-                           (CatListener new-results-so-far (next parser-sequence) node-key))
-          (push-stack tramp (ParseTask. (first parser-sequence) continue-index)))
+        (when (push-listener tramp [continue-index (first parser-sequence)]
+                           (CatListener new-results-so-far (next parser-sequence) node-key tramp grammar))
+          (push-stack tramp #(uparse grammar (first parser-sequence) continue-index tramp)))
         (push-result tramp node-key (make-success new-results-so-far continue-index))))))
 
 ; The top level listener
 
-(def TopListener 
-  (fn [result tramp] 
+(defn TopListener [tramp] 
+  (fn [result] 
     (when (total-success? tramp result)
       (swap! (:success tramp) conj result))))
 
 ;; Parsers
 
-(extend-type String Parser
-  (-parse [string index tramp]
-    (let [text (:text tramp)
-          end (min (count text) (+ index (count string)))
-          head (subs text index end)]      
-      (if (= string head)
-        (success tramp [index string] string end)
-        (fail tramp index)))))
+(defn string-parse
+  [this index tramp]
+  (let [string (:string this)
+        text (:text tramp)
+        end (min (count text) (+ index (count string)))
+        head (subs text index end)]      
+    (if (= string head)
+      (success tramp [index this] string end)
+      (fail tramp index))))
 
-(defrecord Cat [parsers]
-  Parser
-  (-parse [this index tramp]
+(defn cat-parse
+  [grammar this index tramp]
+  (let [parsers (:parsers this)]
     ; Kick-off the first parser, with a CatListener ready to pass the result on in the chain
     ; and with a final target of notifying this parser when the whole sequence is complete
-    (push-listener tramp [index (first parsers)] (CatListener [] (next parsers) [index this]))
-    (push-stack tramp (ParseTask. (first parsers) index))))
+    (when (push-listener tramp [index (first parsers)] (CatListener [] (next parsers) [index this] tramp grammar))
+      (push-stack tramp #(uparse grammar (first parsers) index tramp)))))
 
-(defrecord Alt [parsers]
-  Parser
-  (-parse [this index tramp]
+(defn alt-parse
+  [grammar this index tramp]
+  (let [parsers (:parsers this)]
     (doseq [parser parsers]
-      (push-listener tramp [index parser] (NodeListener [index this]))
-      (push-stack tramp (ParseTask. parser index)))))
+      (when (push-listener tramp [index parser] (NodeListener [index this] tramp))
+        (push-stack tramp #(uparse grammar parser index tramp))))))
 
-(def Epsilon
-  (reify Parser
-    (-parse [this index tramp] (success tramp [index this] nil index))))
+(defn non-terminal-parse
+  [grammar this index tramp]
+  (let [parser (get-parser grammar this)]
+    (when (push-listener tramp [index parser] (NodeListener [index this] tramp))
+      (push-stack tramp #(uparse grammar parser index tramp)))))
 
-(def End
-  (reify Parser
-    (-parse [this index tramp] (if (= index (count (:text tramp)))
-                                 (success tramp [index this] nil index)
-                                 (fail tramp index)))))
+(def Epsilon {:tag :epsilon})
+(defn epsilon-parse
+  [index tramp] (success tramp [index Epsilon] nil index))
+
+(def End {:tag :end})
+(defn end-parse
+  [index tramp] 
+  (if (= index (count (:text tramp)))
+    (success tramp [index End] nil index)
+    (fail tramp index)))
     
-(extend-type clojure.lang.Var
-  Parser
-  (-parse [this index tramp]
-    (-parse (deref this) index tramp)))
+;(extend-type clojure.lang.Delay
+;  Parser
+;  (-parse [this index tramp]
+;    (-parse (deref this) index tramp)))
 
-(defn alt [& parsers] (Alt. parsers))
-(defn cat [& parsers] (Cat. parsers))
+(defn alt [& parsers] {:tag :alt :parsers parsers})
+(defn cat [& parsers] {:tag :cat :parsers parsers})
+(defn string [s] {:tag :string :string s})
 
-(defn parse [parser text]
+(defn parse [grammar parser text]
   (let [tramp (make-tramp text)]
-    (push-listener tramp [0 parser] TopListener)
-    (push-stack tramp (ParseTask. parser 0))
+    (push-listener tramp [0 parser] (TopListener tramp))
+    (push-stack tramp #(uparse grammar parser 0 tramp))
     (run tramp)
     (if @(:success tramp) @(:success tramp) @(:failure tramp))))
 
-(def a (Alt. [(Cat. ["a" #'a]) "a"]))
-(def c (Alt. ["a" "b"]))
-(def b (Alt. ["a" "aa" "aaa"]))
-
-(declare x y)
-(def y "b")
-(def x (cat "a" #'y))
+(def grammar1 {:s (alt (string "a") (string "aa") (string "aaa"))})
+(def grammar2 {:s (alt (string "a") (string "b"))})
+(def grammar3 {:s (alt (cat (string "a") :s) End)})
+(def grammar4 {:y (string "b")
+               :x (cat (string "a") :y)})            
+(def grammar5 {:s (cat (string "a") (string "b") (string "c"))})
+(def grammar6 {:s (alt (cat (string "a") :s) (string "a"))})
+(def grammar7 {:s (alt (cat (string "a") :s) Epsilon)})
+(def grammar8 {:s (alt (cat (string "a") :s End) (string "a"))})
