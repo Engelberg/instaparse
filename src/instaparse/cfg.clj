83a4db5d04f02c9c954ca77d61cb17cd33b7244f
Mark Engelberg <mark.engelberg@gmail.com>
2013-03-18 01:16:14 -0700

    Added support for optional

--- src/instaparse/cfg.clj
+++ src/instaparse/cfg.clj
@@ -1,65 +1,65 @@
 (ns instaparse.cfg
   (:use instaparse.gll))
 
-;syntax     ::=  { rule }
-;rule       ::=  identifier  "::="  expression
-;expression ::=  term { "|" term }
-;term       ::=  factor { factor }
-;factor     ::=  identifier |
-;                quoted_symbol |
-;                "("  expression  ")" |
-;                "["  expression  "]" |
-;                "{"  expression  "}"
-;identifier ::=  letter { letter | digit }
-;quoted_symbol ::= """ { any_character } """
-
 (def single-quoted-string "'(\\'|[^\\'])*'")
 (def single-quoted-regexp "#'(\\'|[^\\'])*'")
 (def double-quoted-string "\"(\\\"|[^\\\"])*\"")
 (def double-quoted-regexp "#\"(\\\"|[^\\\"])*\"")
+(def opt-whitespace (hide (nt :opt-whitespace)))
 
-(def cfg {:rules (cat (hide (nt :opt-whitespace))
+(def cfg {:rules (cat opt-whitespace
                       (plus (nt :rule)))
-          :whitespace (regexp "\\s+")
-          :opt-whitespace (regexp "\\s*")
+          :whitespace (regexp "[,\\s]+")
+          :opt-whitespace (regexp "[,\\s]*")
           :rule-separator (alt (string ":")
                                (string ":=")
                                (string "::=")
                                (string "="))
           :rule (cat (nt :nt) 
-                     (hide (nt :opt-whitespace))
+                     opt-whitespace
                      (hide (nt :rule-separator))
-                     (hide (nt :opt-whitespace))
+                     opt-whitespace
                      (nt :alt)
                      (hide (alt (nt :opt-whitespace)
                                 (regexp "\\s*;\\s*"))))          
-          :nt (regexp "[^ \\r\\t\\n(){}\\[\\]+*?:=|'\"#]+")
+          :nt (regexp "[^, \\r\\t\\n(){}\\[\\]+*?:=|'\"#]+")
           :alt (cat (nt :cat)                           
                     (star
                       (cat
-                        (hide (nt :opt-whitespace))
+                        opt-whitespace
                         (hide (string "|"))
-                        (hide (nt :opt-whitespace))
+                        opt-whitespace
                         (nt :cat))))
-          :paren-alt (cat (hide (string "("))
-                          (hide (nt :opt-whitespace))
-                          (nt :alt)
-                          (hide (nt :opt-whitespace))
-                          (hide (string ")")))
+          :paren (cat (hide (string "("))
+                      opt-whitespace
+                      (nt :alt)
+                      opt-whitespace
+                      (hide (string ")")))
           :cat (plus (cat
-                       (hide (nt :opt-whitespace))
+                       opt-whitespace
                        (nt :factor)
-                       (hide (nt :opt-whitespace))))
+                       opt-whitespace))
           :string (alt
                     (regexp single-quoted-string)
                     (regexp double-quoted-string))
           :regexp (alt
                     (regexp single-quoted-regexp)
                     (regexp double-quoted-regexp))
-          :factor (alt (nt :nt)
-                       (nt :string)
-                       (nt :regexp)                       
-                       (nt :paren-alt))})
+          :opt (alt
+                 (cat (hide (string "["))
+                      opt-whitespace
+                      (nt :alt)
+                      opt-whitespace
+                      (hide (string "]")))
+                 (cat (nt :factor)
+                      opt-whitespace
+                      (hide (string "?"))))
+          :factor (red (alt (nt :nt)
+                            (nt :string)
+                            (nt :regexp)
+                            (nt :opt)                       
+                            (nt :paren))
+                       identity)})
 
 (def cfg1 "S = 'a'")
 (def cfg2 
@@ -73,3 +73,9 @@
 (def cfg4
   "S := A B | C
    C := (A | B) C")
+(def cfg5
+  "S=A?")
+(def cfg6
+  "S =(A | B)?")
+(def cfg7
+  "S = A, B, C, D")
