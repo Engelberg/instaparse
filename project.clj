66fa76dd0d7a5ba7de7a44619780e068deb59662
Mark Engelberg <mark.engelberg@gmail.com>
2013-03-31 12:32:27 -0700

    Edited project.clj

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject instaparse "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject instaparse "0.9.0"
+  :description "Instaparse: No grammar left behind"
+  :url "https://github.com/Engelberg/instaparse"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [criterium "0.3.1"]]                      
+                 [enlive "1.1.1"]]                      
   :dev-dependencies [[lein-marginalia "0.7.1"]
                      [criterium "0.3.1"]])
