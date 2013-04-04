14f47934e8893df36cb219e8c713a32b1c65ce67
Mark Engelberg <mark.engelberg@gmail.com>
2013-04-04 00:03:53 -0700

    Changed readme to version 0.9.0

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "https://github.com/Engelberg/instaparse"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [enlive "1.1.1"]
-                 [org.clojure/tools.trace "0.7.5"]]                      
+  :dependencies [[org.clojure/clojure "1.5.1"]]                      
   :dev-dependencies [[lein-marginalia "0.7.1"]
+                     [enlive "1.1.1"]
+                     [org.clojure/tools.trace "0.7.5"]
                      [criterium "0.3.1"]])
