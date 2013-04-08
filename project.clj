8605001f464922ef6aabb0f42d5e2b125c94ec50
Mark Engelberg <mark.engelberg@gmail.com>
2013-04-08 02:42:37 -0700

    Tweaked project

--- project.clj
+++ project.clj
@@ -3,10 +3,9 @@
   :url "https://github.com/Engelberg/instaparse"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]                      
-  :dev-dependencies [[lein-marginalia "0.7.1"]
-                     [enlive "1.1.1"]
-                     [org.clojure/tools.trace "0.7.5"]
-                     [criterium "0.3.1"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :profiles {:dev {:dependencies 
+                   [[org.clojure/tools.trace "0.7.5"]
+                    [criterium "0.3.1"]]}}
   :scm {:name "git"
         :url "https://github.com/Engelberg/instaparse"})
