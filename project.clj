e18628b09bbbe774f4bf8c200dbaaa908b968581
Mark Engelberg <mark.engelberg@gmail.com>
2013-04-10 16:00:09 -0700

    Starting 1.1 branch

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject instaparse "1.0.0"
+(defproject instaparse "1.1.0-SNAPSHOT"
   :description "Instaparse: No grammar left behind"
   :url "https://github.com/Engelberg/instaparse"
   :license {:name "Eclipse Public License"
