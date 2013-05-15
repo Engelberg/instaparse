7fdff68ad2bbcba0be39c7a9f53f2571a987931e
sam atman <atmanistan@gmail.com>
2013-05-15 13:25:38 -0700

    removed test grammar and call of tree-viz

--- src/instaparse/viz.clj
+++ src/instaparse/viz.clj
@@ -1,16 +1,7 @@
 (ns instaparse.viz
     (:require [rhizome.viz :refer [view-tree]]
               [instaparse.core :refer [parser parse parses]]))
-                                       
-              
-(def make-tree
-     "simple tree parser"
-     (parser "tree: node* 
-              node: leaf | <'('> node (<'('> node <')'>)* node* <')'> 
-              leaf: #'a+'
-              "))
-              
-(def a-tree (make-tree "aaaa(aaaa(aaa(aa)aa)aaa)aaaaa"))  
+ 
               
 (defn- acceptable? [node]
        (not (keyword? node)))
@@ -25,6 +16,4 @@
                :node->descriptor (fn [n] {:label (if (vector? n) 
                                                      (first n) 
                                                      (when (string? n) n ))})))
-
-(tree-viz a-tree)
                                    
