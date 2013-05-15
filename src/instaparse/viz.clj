887de513053ff5c3e76b957416eddcbebcc81f82
sam atman <atmanistan@gmail.com>
2013-05-15 16:33:28 -0700

    On branch master

--- src/instaparse/viz.clj
+++ src/instaparse/viz.clj
@@ -4,7 +4,7 @@
 (defn tree-viz 
     "visualize instaparse hiccup output as a rhizome graph. Requires rhizome: https://github.com/ztellman/rhizome"
     [mytree]
-    (try (use 'rhizome.viz) (catch Exception e (println (str (.getMessage e) " to install rhizome, "
+    (try (require '[rhizome.viz :refer [view-tree]]) (catch Exception e (println (str (.getMessage e) " to install rhizome, "
                                                              "visit https://github.com/ztellman/rhizome "
                                                              "and follow the instructions for your platform."))))
     (rhizome.viz/view-tree sequential? rest mytree 
