67c226d7295b4205a5f5b6217901bd111669caab
sam atman <atmanistan@gmail.com>
2013-05-15 13:34:42 -0700

    algorithm simplified courtesy @Engelberg

--- src/instaparse/viz.clj
+++ src/instaparse/viz.clj
@@ -1,18 +1,11 @@
 (ns instaparse.viz
-    (:require [rhizome.viz :refer [view-tree]]
-              #_[instaparse.core :refer [parser parse parses]]))
- 
-              
-(defn- acceptable? [node]
-       (not (keyword? node)))
-              
-(defn- seq-for-labels [tree]
-            (filter acceptable? tree))
+    (:require [rhizome.viz :refer [view-tree]]))
+            
               
 (defn tree-viz 
-    "visualize instaparse hiccup output as a rhizome graph" 
-    [tree]
-    (view-tree sequential? seq-for-labels tree 
+    "visualize instaparse hiccup output as a rhizome graph"
+    [mytree]
+    (view-tree sequential? rest mytree 
                :node->descriptor (fn [n] {:label (if (vector? n) 
                                                      (first n) 
                                                      (when (string? n) n ))})))
