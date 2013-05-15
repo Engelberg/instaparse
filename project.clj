025c36818ae8f05b03ff42ce4e2e70dd8e5d23c8
sam atman <atmanistan@gmail.com>
2013-05-15 14:03:16 -0700

    try/catch without namespace and project dependency fails to load rhizome.viz  On branch master

--- project.clj
+++ project.clj
@@ -3,8 +3,7 @@
   :url "https://github.com/Engelberg/instaparse"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [rhizome "0.1.6"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]]
   :profiles {:dev {:dependencies 
                    [[org.clojure/tools.trace "0.7.5"]
                     [criterium "0.3.1"]]}}
--- src/instaparse/viz.clj
+++ src/instaparse/viz.clj
@@ -4,7 +4,9 @@
 (defn tree-viz 
     "visualize instaparse hiccup output as a rhizome graph. Requires rhizome: https://github.com/ztellman/rhizome"
     [mytree]
-    (try (use 'rhizome.viz) (catch Exception e (println (str "caught exception: " (.getMessage e)))))
+    (try (use 'rhizome.viz) (catch Exception e (println (str (.getMessage e) " to install rhizome, "
+                                                             "visit https://github.com/ztellman/rhizome "
+                                                             "and follow the instructions for your platform."))))
     (rhizome.viz/view-tree sequential? rest mytree 
                :node->descriptor (fn [n] {:label (if (vector? n) 
                                                      (first n) 
