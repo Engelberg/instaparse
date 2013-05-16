66aa0730ae0df724a12031e55077f342b078750b
Mark Engelberg <mark.engelberg@gmail.com>
2013-05-15 17:15:35 -0700

    Clever trick to handle missing dependency.

--- src/instaparse/viz.clj
+++ src/instaparse/viz.clj
@@ -1,13 +1,14 @@
-(ns instaparse.viz)
-            
-              
+(ns instaparse.viz)   
+
+(try 
+  (require '[rhizome.viz :as r])
+  (catch Exception e
+    (require '[instaparse.viz-not-found :as r])))
+
 (defn tree-viz 
     "visualize instaparse hiccup output as a rhizome graph. Requires rhizome: https://github.com/ztellman/rhizome"
-    [mytree]
-    (try (use 'rhizome.viz) (catch Exception e (println (str (.getMessage e) " to install rhizome, "
-                                                             "visit https://github.com/ztellman/rhizome "
-                                                             "and follow the instructions for your platform."))))
-    (rhizome.viz/view-tree sequential? rest mytree 
-               :node->descriptor (fn [n] {:label (if (vector? n) 
-                                                     (first n) 
-                                                     (when (string? n) n ))})))
+    [mytree]           
+      (r/view-tree sequential? rest mytree 
+                   :node->descriptor (fn [n] {:label (if (vector? n) 
+                                                       (first n) 
+                                                       (when (string? n) n ))})))      
