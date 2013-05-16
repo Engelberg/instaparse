e8d88b0e9d2e8fb436ffa30990da959dec766a91
sam atman <atmanistan@gmail.com>
2013-05-16 11:54:54 -0700

    added enlive visualizer and supporting functions. NB: the namespace trick does not succeed in loading rhizome on my system, where it is in fact installed.
     On branch v1.2

--- src/instaparse/viz.clj
+++ src/instaparse/viz.clj
@@ -7,28 +7,8 @@
 
 (defn tree-viz 
     "visualize instaparse hiccup output as a rhizome graph. Requires rhizome: https://github.com/ztellman/rhizome"
-<<<<<<< HEAD
-    [mytree]
-<<<<<<< HEAD
-    (try (use 'rhizome.viz) (catch Exception e (println (str (.getMessage e) " to install rhizome, "
-=======
-    (try (require '[rhizome.viz :refer [view-tree]]) (catch Exception e (println (str (.getMessage e) " to install rhizome, "
->>>>>>> master
-                                                             "visit https://github.com/ztellman/rhizome "
-                                                             "and follow the instructions for your platform."))))
-    (rhizome.viz/view-tree sequential? rest mytree 
-               :node->descriptor (fn [n] {:label (if (vector? n) 
-                                                     (first n) 
-<<<<<<< HEAD
-                                                     (when (string? n) n ))})))
-=======
-                                                     (when (string? n) n ))})))
-                                   
->>>>>>> master
-=======
     [mytree]           
       (r/view-tree sequential? rest mytree 
                    :node->descriptor (fn [n] {:label (if (vector? n) 
                                                        (first n) 
-                                                       (when (string? n) n ))})))      
->>>>>>> 66aa0730ae0df724a12031e55077f342b078750b
+                                                       (when (string? n) n ))})))
