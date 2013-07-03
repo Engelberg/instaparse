400fee34d63557095d07d36f7e591efab4730c2e
Mark Engelberg <mark.engelberg@gmail.com>
2013-07-03 01:29:04 -0700

    Changed failure signal to global def

--- src/instaparse/repeat.clj
+++ src/instaparse/repeat.clj
@@ -11,12 +11,14 @@
   (or (and (vector? result) (= (count result) 1))
       (and (map? result) (contains? result :tag) (empty? (get result :contents)))))        
 
+(def ^:constant failure-signal (gll/->Failure nil nil))
+
 (defn parse-from-index [grammar initial-parser text segment index]
   (let [tramp (gll/make-tramp grammar text segment)]
     (gll/push-listener tramp [index initial-parser] (gll/TopListener tramp))
     (if-let [all-parses (gll/run tramp)]
       (apply max-key (comp second viz/span) all-parses) 
-      (fail/augment-failure @(:failure tramp) text))))
+      failure-signal)))
 
 (defn repeat-parse-hiccup [grammar initial-parser root-tag text]
   (let [length (count text)
@@ -29,10 +31,10 @@
                               :instaparse.gll/end-index length})
         (let [first-result (parse-from-index grammar initial-parser text segment index)
               [start end] (viz/span first-result)
-              end (if end end (+ index (count first-result)))]
+              end (if end (long end) (+ index (count first-result)))]
           (cond 
             (instance? instaparse.gll.Failure first-result) first-result
-            (= index end) (gll/->Failure nil nil)
+            (= index end) failure-signal
             :else (recur end (afs/conj-flat parses first-result))))))))
           
 (defn repeat-parse-enlive [grammar initial-parser root-tag text]
@@ -46,10 +48,10 @@
                               :instaparse.gll/end-index length})
         (let [first-result (parse-from-index grammar initial-parser text segment index)
               [start end] (viz/span first-result)
-              end (if end end (+ index (count first-result)))]
+              end (if end (long end) (+ index (count first-result)))]
           (cond 
             (instance? instaparse.gll.Failure first-result) first-result
-            (= index end) (gll/->Failure nil nil)
+            (= index end) failure-signal
             :else (recur end (afs/conj-flat parses first-result))))))))
 
 (defn repeat-parse-no-tag [grammar initial-parser text]
@@ -63,10 +65,10 @@
                               :instaparse.gll/end-index length})
         (let [first-result (parse-from-index grammar initial-parser text segment index)
               [start end] (viz/span first-result)
-              end (if end end (+ index (count first-result)))]
+              end (if end (long end) (+ index (count first-result)))]
           (cond 
             (instance? instaparse.gll.Failure first-result) first-result
-            (= index end) (gll/->Failure nil nil)
+            (= index end) failure-signal
             :else (recur end (afs/conj-flat parses first-result))))))))
 
 (defn repeat-parse 
@@ -85,7 +87,7 @@
         output-format (:output-format parser)
         start-rule (get grammar start-production)]
     (cond
-      (= (:hide start-rule) true) (gll/->Failure nil nil)
+      (= (:hide start-rule) true) failure-signal
       (= (:red start-rule) red/raw-non-terminal-reduction)
       (cond
         (= (:tag start-rule) :star)
@@ -93,18 +95,18 @@
         (= (:tag start-rule) :plus)
         (let [result (repeat-parse grammar (:parser start-rule) output-format text)]
           (if (empty? result)
-            (gll/->Failure nil nil) ; simply a failure signal
+            failure-signal ; simply a failure signal
             result))
-        :else (gll/->Failure nil nil))
+        :else failure-signal)
               
       (= (:tag start-rule) :star)
       (repeat-parse grammar (:parser start-rule) output-format start-production text)
       (= (:tag start-rule) :plus)
       (let [result (repeat-parse grammar (:parser start-rule) output-format start-production text)]
         (if (empty-result? result)
-          (gll/->Failure nil nil) ; simply a failure signal
+          failure-signal ; simply a failure signal
           result))
-      :else (gll/->Failure nil nil))))
+      :else failure-signal)))
            
           
     
