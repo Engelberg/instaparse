82265bc4a1a5da5b748f517a2e6d403d002616d5
Ryan Berdeen <ryan@ryanberdeen.com>
2013-12-07 17:52:04 -0500

    update clojurescript and lein-cljsbuild

--- project.clj
+++ project.clj
@@ -4,11 +4,11 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-1913"]
+                 [org.clojure/clojurescript "0.0-2030"]
                  [com.cemerick/clojurescript.test "0.0.4"]]
 
   :source-paths ["src/clj"] 
-  :plugins [[lein-cljsbuild "0.3.3"]]
+  :plugins [[lein-cljsbuild "1.0.0"]]
   :profiles {:dev {:dependencies 
                    [[org.clojure/tools.trace "0.7.5"]
                     [criterium "0.3.1"]
