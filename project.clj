73f3f9092a0cd549d14c9ec0166dcebdd733d732
Lucas Bradstreet <lucasbradstreet@gmail.com>
2016-04-25 18:24:34 +0700

    Bump for snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.lucasbradstreet/instaparse-cljs "1.4.1.2"
+(defproject com.lucasbradstreet/instaparse-cljs "1.4.1.3-SNAPSHOT"
   :description "Instaparse: No grammar left behind"
   :url "https://github.com/lbradstreet/instaparse-cljs"
   :license {:name "Eclipse Public License"
