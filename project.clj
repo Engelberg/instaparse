209c38cec255e922bbcb9f201ca30589730d9add
Mark Engelberg <mark.engelberg@gmail.com>
2018-04-08 19:11:30 -0700

    Updated project to Clojure 1.9

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject instaparse "1.4.8"
+(defproject instaparse "1.4.9"
   :description "Instaparse: No grammar left behind"
   :url "https://github.com/Engelberg/instaparse"
   :license {:name "Eclipse Public License"
@@ -15,9 +15,9 @@
                                   [org.clojure/clojurescript "1.7.28"]]}
              :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]
                                   [org.clojure/clojurescript "1.8.34"]]}
-             :1.9 {:dependencies [[org.clojure/clojure "1.9.0-alpha19"]
-                                  [org.clojure/clojurescript "1.9.908"]
-                                  [org.clojure/tools.reader "1.0.3"]]}}
+             :1.9 {:dependencies [[org.clojure/clojure "1.9.0"]
+                                  [org.clojure/clojurescript "1.10.238"]
+                                  [org.clojure/tools.reader "1.2.1"]]}}
   :aliases {"test-all" ["with-profile" "+1.5:+1.6:+1.7:+1.8:+1.9" "test"]
             "test-cljs" ["cljsbuild" "test" "unit-tests"]
             "test-cljs-all" ["with-profile" "+1.7:+1.8:+1.9" "do" "clean," "test-cljs"]}
