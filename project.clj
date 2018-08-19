864793c0bbe6f9ecb61910f49253229bccb79764
Alex Engelberg <alex.benjamin.engelberg@gmail.com>
2018-08-19 15:55:32 -0700

    Rip out cljs-related config and irrelevant tests

--- project.clj
+++ project.clj
@@ -3,56 +3,7 @@
   :url "https://github.com/Engelberg/instaparse"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
-  :profiles {:dev {:dependencies
-                   [[org.clojure/clojurescript "1.8.40"]
-                    [org.clojure/tools.trace "0.7.9"]
-                    [criterium "0.4.4"]
-                    [rhizome "0.2.9"]]}
-             :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
-             :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
-             :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]
-                                  [org.clojure/clojurescript "1.7.28"]]}
-             :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]
-                                  [org.clojure/clojurescript "1.8.34"]]}
-             :1.9 {:dependencies [[org.clojure/clojure "1.9.0"]
-                                  [org.clojure/clojurescript "1.10.238"]
-                                  [org.clojure/tools.reader "1.2.1"]]}
-             :cloverage {:cljsee ^:replace
-                         {:builds [{:source-paths ["src/"]
-                                    :output-path "target/generated/src/clj"
-                                    :rules :clj}]}}}
-  :aliases {"test-all" ["with-profile" "+1.5:+1.6:+1.7:+1.8:+1.9" "test"]
-            "test-cljs" ["cljsbuild" "test" "unit-tests"]
-            "test-cljs-all" ["with-profile" "+1.7:+1.8:+1.9" "do" "clean," "test-cljs"]
-            "coverage" ["with-profile" "+cloverage" "do" "clean," "cljsee," "cloverage"]}
-  :test-paths ["test/" "target/generated/test/clj"]
-  :source-paths ["src/" "target/generated/src/clj"]
-  :cljsee {:builds [{:source-paths ["src/"]
-                     :output-path "target/generated/src/clj"
-                     :rules :clj}
-                    {:source-paths ["test/"]
-                     :output-path "target/generated/test/clj"
-                     :rules :clj}]}
-  :plugins [[lein-cljsbuild "1.1.7"]
-            [cljsee "0.1.0"]
-            [lein-cloverage "1.0.13"]]
-  ;:hooks [leiningen.cljsbuild]
-  :target-path "target"
+  :dependencies [[org.clojure/clojure "1.9.0"]]
+  :plugins [[lein-cloverage "1.0.13"]]
   :scm {:name "git"
-        :url "https://github.com/Engelberg/instaparse"}
-  :prep-tasks [["cljsee" "once"]]
-  :cljsbuild {:builds [{:id "none"
-                        :source-paths ["src/"]
-                        :compiler {:output-to "target/js/none.js"
-                                   :optimizations :none
-                                   :pretty-print true}}
-                       {:id "test"
-                        :source-paths ["src/"
-                                       "test/"
-                                       "runner/cljs"]
-                        :compiler {:output-to "target/js/advanced-test.js"
-                                   :optimizations :advanced
-                                   :target :nodejs
-                                   :pretty-print false}}]
-              :test-commands {"unit-tests" ["node" "target/js/advanced-test.js"]}})
+        :url "https://github.com/Engelberg/instaparse"})
